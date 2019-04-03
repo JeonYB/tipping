120468b5460a1d22f30d0a0705bb26f5aa9f9893
Jiwoon_kim <cyon365@gmail.com>
2019-04-03 18:30:01 +0900

    “{update}”

--- public/js/common/common.js
+++ public/js/common/common.js
@@ -9,8 +9,23 @@
         }
     });
   };
-
+  
+  layerPopup = function() {
+    var layerButton = $('.layerpopup-button');
+    var layerSection = $('.section-layer')
+    if($('a').hasClass('layerpopup-button')){
+      layerButton.each(function(i, e){
+        $(this).click(function(){
+          layerSection.eq(i).addClass('on');
+          layerSection.eq(i).parents('div').append('<div class="dim-layer"/>')  
+        })
+      })
+    } else {
+      return false;
+    }
+  }
   $(document).ready(function() {
     headerScroll();
+    layerPopup()
   });
 })(jQuery);
--- router/main.js
+++ router/main.js
@@ -1,10 +1,17 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
+   /* 메인  */
+   app.get('/',function(req,res){
+      res.render('index.html')
+   });
 
-     app.get('/account',function(req,res){
+   /* 계정신청  */
+   app.get('/account',function(req,res){
       res.render('account.html')
+   });
+
+   /* 내정보  */
+   app.get('/mypage',function(req,res){
+      res.render('mypage.html')
    });
 }
